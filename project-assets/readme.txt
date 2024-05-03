c6b5eb64d62abfc51dcd1806ed072ed3073bb6bf
Intancote <sepandiusa@gmail.com>
2024-05-03 10:06:03 +0200

    chore: add and rename assets, add license and update PlayState and Project.xml

---
+++ project-assets/readme.txt
@@ -0,0 +1,15 @@
+ __  __ _                                               _
+|  \/  (_)_ _  ___ ____ __ _____ _ __ _ __  ___ _ _   _| |_
+| |\/| | | ' \/ -_|_-< V  V / -_) '_ \ '_ \/ -_) '_| |_   _|
+|_|  |_|_|_||_\___/__/\_/\_/\___| .__/ .__/\___|_|     |_|
+                                |_|  |_|
+
+This program is still a work in progress and will be updated as time goes on!
+Currently this version in particular is a preview alpha build of the program and may contain bugs.
+If you find any bugs in this version, please create a issue on github and/or contribute to the project
+via pull requests to further increase the amount of features and/or increase performance.
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,3 +1,4 @@
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.graphics.frames.FlxAtlasFrames;
@@ -10,13 +11,53 @@ class PlayState extends FlxState
 	{
 		super.create();
 
-		var testSprite = new FlxSprite();
-		testSprite.loadGraphic("assets/images/Unrevealed-Tile.png",);
-		add(testSprite);
+		// Define grid size
+		var gridRows:Int = 9;
+		var gridColumns:Int = 9;
+
+		// Define sprite size
+		var spriteWidth:Int = 64;
+		var spriteHeight:Int = 64;
+
+		// Calculate the starting position for the first sprite
+		var startX:Float = (FlxG.width - (gridColumns * spriteWidth)) / 2;
+		var startY:Float = (FlxG.height - (gridRows * spriteHeight)) / 2;
+
+		// Create sprites in a loop
+		for (i in 0...gridRows)
+		{
+			for (j in 0...gridColumns)
+			{
+				var sprite = new FlxSprite(startX + j * spriteWidth, startY + i * spriteHeight);
+				sprite.loadGraphic("assets/images/Tile-Unrevealed.png");
+				add(sprite);
+			}
+		}
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		// Check if the mouse is pressed
+		if (FlxG.mouse.justPressed)
+		{
+			// Get the mouse position
+			var mouseX:Float = FlxG.mouse.screenX;
+			var mouseY:Float = FlxG.mouse.screenY;
+
+			// Loop through all the sprites (tiles) in the state
+			for (sprite in members)
+			{
+				if (FlxG.mouse.overlaps(sprite))
+				{
+					/* sprite.loadGraphic("assets/images/Tile-Empty.png"); */
+					// Perform an action when a tile is clicked, e.g., reveal the tile
+					// This is a placeholder for your actual logic
+					/* trace("Tile clicked: " + sprite.x + ", " + sprite.y); */
+					// You can add your logic here to reveal the tile or perform other actions
+				}
+			}
+		}
 	}
 }
